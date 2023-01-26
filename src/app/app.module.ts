fd5b7640724e6830cb13d1d2b91813d6170f874f
Janmajaya <janmajayagantayat@gmail.com>
2023-01-26 22:53:37 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipesListComponent } from './recipe/recipes-list/recipes-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipe/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    RecipesListComponent,
+    HeaderComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
